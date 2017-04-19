6603cabf64f44bbe695d9b52b5dc30934297f6da
phith0n <phith0n.ph2f@gmail.com>
2017-04-19 23:01:37 +0800

    imagetragick init

---
+++ imagetragick/www/demo.php
@@ -0,0 +1,2 @@
+<?php
+echo shell_exec("/usr/local/bin/identify vul.jpg");
---
+++ imagetragick/www/upload.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!empty($_FILES)):
+
+$ext = pathinfo($_FILES['file_upload']['name'], PATHINFO_EXTENSION);
+if (!in_array($ext, ['gif', 'png', 'jpg', 'jpeg'])) {
+    die('Unsupported filetype uploaded.');
+}
+
+$i = new Imagick($_FILES['file_upload']['tmp_name']);
+$size = $i->getSize();
+
+echo "Image size is: ";
+print_r($size);
+
+else:
+?>
+<form method="post" enctype="multipart/form-data">
+    File: <input type="file" name="file_upload">
+    <input type="submit">
+</form>
+<?php
+endif;
