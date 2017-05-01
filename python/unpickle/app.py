16e8fa9b480aa4ccdebb34007c7fc17fc70a03e3
phith0n <phith0n.ph2f@gmail.com>
2017-05-02 01:30:17 +0800

    remove protocol from pickle.loads, the protocol will be detected automatically.

--- python/unpickle/app.py
+++ python/unpickle/app.py
@@ -5,11 +5,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<int:version>")
-def index(version=3):
+def index():
     try:
         user = base64.b64decode(request.cookies.get('user'))
-        user = pickle.loads(user, protocol=version)
+        user = pickle.loads(user)
         username = user["username"]
     except:
         username = "Guest"
