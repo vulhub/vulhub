67699ce2001a0f4da7ead8e829fb3690a722660e
phith0n <phith0n.ph2f@gmail.com>
2017-05-15 21:35:22 +0800

    add redis server

---
+++ weblogic/ssrf/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+	set -- redis-server "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+	chown -R redis .
+	exec gosu redis "$0" "$@"
+fi
+
+cron
+
+exec "$@"
