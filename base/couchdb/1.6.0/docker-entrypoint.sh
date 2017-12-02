933167a9a40e65ae50ac07ebcdd851b2c0db1b78
phith0n <phith0n.ph2f@gmail.com>
2017-12-02 22:17:07 +0800

    use official dockerfile

--- base/couchdb/1.6.0/docker-entrypoint.sh
+++ base/couchdb/1.6.0/docker-entrypoint.sh
@@ -14,31 +14,49 @@
 set -e
 
 if [ "$1" = 'couchdb' ]; then
-    chown -R couchdb:couchdb \
-		/usr/var/lib/couchdb \
-		/usr/var/log/couchdb \
-		/usr/var/run/couchdb \
-		/usr/etc/couchdb
+	# we need to set the permissions here because docker mounts volumes as root
+	chown -R couchdb:couchdb \
+		/usr/local/var/lib/couchdb \
+		/usr/local/var/log/couchdb \
+		/usr/local/var/run/couchdb \
+		/usr/local/etc/couchdb
 
 	chmod -R 0770 \
-		/usr/var/lib/couchdb \
-		/usr/var/log/couchdb \
-		/usr/var/run/couchdb \
-		/usr/etc/couchdb
+		/usr/local/var/lib/couchdb \
+		/usr/local/var/log/couchdb \
+		/usr/local/var/run/couchdb \
+		/usr/local/etc/couchdb
 
-	chmod 664 /usr/etc/couchdb/*.ini
-	chmod 775 /usr/etc/couchdb/*.d
+	chmod 664 /usr/local/etc/couchdb/*.ini
+	chmod 775 /usr/local/etc/couchdb/*.d
 
 	if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
 		# Create admin
-		printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /usr/etc/couchdb/local.d/docker.ini
-        chown couchdb:couchdb /usr/etc/couchdb/local.d/docker.ini
+		printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /usr/local/etc/couchdb/local.d/docker.ini
+		chown couchdb:couchdb /usr/local/etc/couchdb/local.d/docker.ini
 	fi
 
-	printf "[httpd]\nport = %s\nbind_address = %s\n" ${COUCHDB_HTTP_PORT:=5984} ${COUCHDB_HTTP_BIND_ADDRESS:=0.0.0.0} > /usr/etc/couchdb/local.d/bind_address.ini
-    chown couchdb:couchdb /usr/etc/couchdb/local.d/bind_address.ini
+	printf "[httpd]\nport = %s\nbind_address = %s\n" ${COUCHDB_HTTP_PORT:=5984} ${COUCHDB_HTTP_BIND_ADDRESS:=0.0.0.0} > /usr/local/etc/couchdb/local.d/bind_address.ini
+	chown couchdb:couchdb /usr/local/etc/couchdb/local.d/bind_address.ini
 
-    exec gosu couchdb "$@"
+	# if we don't find an [admins] section followed by a non-comment, display a warning
+	if ! grep -Pzoqr '\[admins\]\n[^;]\w+' /usr/local/etc/couchdb; then
+		# The - option suppresses leading tabs but *not* spaces. :)
+		cat >&2 <<-'EOWARN'
+			****************************************************
+			WARNING: CouchDB is running in Admin Party mode.
+			         This will allow anyone with access to the
+			         CouchDB port to access your database. In
+			         Docker's default configuration, this is
+			         effectively any other container on the same
+			         system.
+			         Use "-e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password"
+			         to set it in "docker run".
+			****************************************************
+		EOWARN
+	fi
+
+	exec gosu couchdb "$@"
 fi
 
 exec "$@"
