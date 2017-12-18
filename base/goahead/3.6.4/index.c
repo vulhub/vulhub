0b7a04e9fb09c174341a9e86584c65179d4c2aac
phith0n <phith0n.ph2f@gmail.com>
2017-12-19 01:19:15 +0800

    index.c

---
+++ base/goahead/3.6.4/index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+    printf("Content-type: text/html\n\n") ;
+    printf("<title>cgi title</title>");
+    printf("<h1>Hello world!</h1>");
+    return 0;
+}
