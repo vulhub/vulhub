3e2884b18246ffbab74c1450509fcb371fd78eab
phith0n <phith0n.ph2f@gmail.com>
2018-03-27 20:20:11 +0800

    move crond

--- docker/unauthorized-rce/docker-entrypoint.sh
+++ docker/unauthorized-rce/docker-entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 # no arguments passed
 # or first arg is `-f` or `--some-option`
 if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
-    # Start crond process
-    crond -b
-
 	# add our default arguments
 	set -- dockerd \
 		--host=unix:///var/run/docker.sock \
@@ -20,4 +17,6 @@ if [ "$1" = 'dockerd' ]; then
 	set -- sh "$(which dind)" "$@"
 fi
 
+# Start crond process
+crond -b
 exec "$@"
