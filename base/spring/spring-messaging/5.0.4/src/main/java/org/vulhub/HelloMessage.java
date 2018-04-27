ff20703a785821e60f6eaee0840ff53d0db6dbcd
phith0n <phith0n.ph2f@gmail.com>
2018-04-27 13:08:02 +0800

    initial spring messaging example application

---
+++ base/spring/spring-messaging/5.0.4/src/main/java/org/vulhub/HelloMessage.java
@@ -0,0 +1,21 @@
+package org.vulhub;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
