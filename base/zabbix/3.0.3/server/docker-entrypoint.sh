121056bb72ff233ffcd99febcaeb2785e9b9637c
phith0n <phith0n.ph2f@gmail.com>
2018-05-02 01:59:45 +0800

    add zabbix 3.0.3 server

---
+++ base/zabbix/3.0.3/server/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+__stop() {
+ echo "> SIGTERM signal received, try to gracefully shutdown all services..."
+ echo "> Stopping: Agent"
+ kill $(cat /var/run/zabbix/zabbix_agentd.pid)
+ tail -n50 /var/log/zabbix/zabbix_agentd.log
+ echo "> Stopping: Server"
+ kill $(cat /var/run/zabbix/zabbix_server.pid)
+ tail -n50 /var/log/zabbix/zabbix_server.log
+}
+
+trap "__stop; exit 0" SIGTERM SIGINT
+
+sed -e "s/.*\(ListenPort=\).*/\1$ZBX_SRV_PORT/g" \
+    -e "s/.*\(DBHost=\).*/\1$DATABASE_HOST/g" \
+    -e "s/^\(DBName=\).*/\1$DATABASE_NAME/g" \
+    -e "s/.*\(DBPort=\).*/\1$DATABASE_PORT/g" \
+    -e "s/^\(DBUser=\).*/\1$DATABASE_USER/g" \
+    -e "s/.*\(DBPassword=\).*/\1$DATABASE_PASS/g" -i /etc/zabbix/zabbix_server.conf
+
+sed -e "s/^\(Server=\).*/\1$ZBX_SRV_HOST/g" \
+    -e "s/^\(ServerActive=\).*/\1$ZBX_SRV_HOST_ACT/g" \
+    -e "s/.*\(StartAgents=\).*/\1$ZBX_AGT_NUM/g" \
+    -e "s/.*\(ListenPort=\).*/\1$ZBX_AGT_PORT/g" \
+    -e "s/^\(Hostname=\).*/\1`hostname`/g" -i /etc/zabbix/zabbix_agentd.conf
+
+echo "> Starting: Server"
+/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
+tail -n100 /var/log/zabbix/zabbix_server.log
+
+echo "> Starting: Agent"
+/usr/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
+tail -n100 /var/log/zabbix/zabbix_agentd.log
+
+while true; do
+ sleep 15s & wait $!;
+ ps -o pid |egrep "^\s+$(cat /var/run/zabbix/zabbix_server.pid)\$" 1>/dev/null || exit 1;
+done
