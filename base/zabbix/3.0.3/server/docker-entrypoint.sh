59c85ba843a7548d2fdf8f48e6758f7bb2ce3438
phith0n <phith0n.ph2f@gmail.com>
2018-05-02 20:36:12 +0800

    add --foreground

--- base/zabbix/3.0.3/server/docker-entrypoint.sh
+++ base/zabbix/3.0.3/server/docker-entrypoint.sh
@@ -20,7 +20,7 @@ if [ "$1" == "server" ]; then
         -e "s/^\(DBUser=\).*/\1$DATABASE_USER/g" \
         -e "s/.*\(DBPassword=\).*/\1$DATABASE_PASS/g" -i /etc/zabbix/zabbix_server.conf
     echo "> Running zabbix server..."
-    exec su zabbix -s "/bin/bash" -c "/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf"
+    exec su zabbix -s "/bin/bash" -c "/usr/sbin/zabbix_server --foreground -c /etc/zabbix/zabbix_server.conf"
 elif [ "$1" == "agent" ]; then
     sed -e "s/^\(Server=\).*/\1$ZBX_SRV_HOST/g" \
         -e "s/^\(ServerActive=\).*/\1$ZBX_SRV_HOST_ACT/g" \
@@ -28,7 +28,7 @@ elif [ "$1" == "agent" ]; then
         -e "s/.*\(ListenPort=\).*/\1$ZBX_AGT_PORT/g" \
         -e "s/^\(Hostname=\).*/\1`hostname`/g" -i /etc/zabbix/zabbix_agentd.conf
     echo "> Running zabbix server..."
-    exec su zabbix -s "/bin/bash" -c "/usr/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf"
+    exec su zabbix -s "/bin/bash" -c "/usr/sbin/zabbix_agentd --foreground -c /etc/zabbix/zabbix_agentd.conf"
 else
     exec "$@"
 fi
