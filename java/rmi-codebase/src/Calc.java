e07bca4aeae9c650b0c0cc9259ddb1aba11204da
phith0n <phith0n.ph2f@gmail.com>
2019-10-03 02:08:10 +0800

    improve rmi-codebase src

--- java/rmi-codebase/src/Calc.java
+++ java/rmi-codebase/src/Calc.java
@@ -3,7 +3,9 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 
 public class Calc extends UnicastRemoteObject implements ICalc {
-    public Calc() throws RemoteException {}
+    public Calc() throws RemoteException {
+        super(64000);
+    }
 
     public Integer sum(List<Integer> params) throws RemoteException {
         Integer sum = 0;
