b652faa3b2832beeb054cbe7642c2c6e5cc1f04e
phith0n <phith0n.ph2f@gmail.com>
2020-08-10 19:34:09 +0800

    add some tests

--- tests/images-build.sh
+++ tests/images-build.sh
@@ -3,21 +3,22 @@
 set -ex
 
 runTest() {
-    local image_path=$1
-    local image_name=$2
+    local image_path="$1"
+    local image_name="$2"
     if [[ ! -d $image_path ]]; then
         echo "error message: image path \"$image_path\" not exists"
         exit 1
     fi
 
     tag=$(date +%s)
-    docker build "$image_path" -t "$image_name:$tag" 
+    cd "$image_path"
+    docker build -t "$image_name:$tag" .
 }
 
 for path in "$@"; do
-    image_path=${path%%/}
-    image_name=${image_path//[\/\-.]/_}
+    image_path="${path%%/}"
+    image_name="${image_path//[\/\-.]/_}"
     runTest "$image_path" "$image_name"
-    # echo "Image Name $image_name"
+    #echo "Image Name $image_name"
 done
 
