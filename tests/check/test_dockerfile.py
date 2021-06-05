3603bc03b73025f1008789c886bbdf630c43727e
phith0n <phith0n.ph2f@gmail.com>
2021-06-05 23:04:48 +0800

    add CI workflow

---
+++ tests/check/test_dockerfile.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+
+def test_dockerfile_lint():
+    basedir = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
+
+    dockerfiles = []
+    for (now_dir, dirs, files) in os.walk(basedir):
+        for name in files:
+            if name in ('oracle-java', ):
+                continue
+
+            if name == 'Dockerfile':
+                dockerfiles.append(os.path.join(now_dir, name))
+
+    config = os.path.join(basedir, 'tests', 'hadolint.yaml')
+    subprocess.run(['hadolint', '--config', config, '--failure-threshold', 'warning'] + dockerfiles, check=True)
