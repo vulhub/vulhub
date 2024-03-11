993348f346d18e6ac0f30c5d953925bdb8c783df
phith0n <phith0n.ph2f@gmail.com>
2024-03-12 02:07:11 +0800

    added Solr 8.6.1 (Cloud mode)

--- base/solr/8.6.1/docker-entrypoint.sh
+++ base/solr/8.6.1/docker-entrypoint.sh
@@ -10,14 +10,13 @@ if [ -d "$CORE_DIR" ]; then
   echo "$CORE_DIR exists; skipping demo creation"
 else
   # start Solr background
-  SOLR_OPTS="-Djetty.host=localhost" solr start -force -p 8983
+  solr start -cloud -force -p 8983 -s /opt/solr/example/cloud
   # wait for Solr startup
   while ! wget -qO- http://localhost:8983 | grep -i solr >/dev/null; do
     echo "waiting for Apache Solr ready..."
     sleep 1
   done
   
-
   echo "Creating $CORE"
   solr create -force -c "$CORE"
   echo "Created $CORE"
@@ -28,13 +27,14 @@ else
   post -c $CORE -commit yes example/exampledocs/books.csv
   echo "Loaded example data"
   # check the core_dir exists; otherwise the detecting above will fail after stop/start
-  if [ ! -d "$CORE_DIR" ]; then
+  if ! wget -qO- "http://localhost:8983/solr/admin/collections?action=list&wt=json" | grep -i "$CORE" >/dev/null; then
     echo "Missing $CORE_DIR"
     exit 1
   fi
 
   # stop Solr background
-  SOLR_OPTS="-Djetty.host=localhost" solr stop -force -p 8983
+  solr stop -force -p 8983
+  mkdir "$CORE_DIR"
 fi
 
 exec "$@"
