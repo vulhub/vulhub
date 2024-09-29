5e5b1688c3546d4819a0b3d26d594b07086757c8
phith0n <phith0n.ph2f@gmail.com>
2024-09-29 15:17:31 +0800

    chore: improved image for cups-browsed 2.0.1

--- base/cups-browsed/2.0.1/docker-entrypoint.sh
+++ base/cups-browsed/2.0.1/docker-entrypoint.sh
@@ -4,11 +4,9 @@ set -e
 
 if [ "$1" = "cups" ]; then
     /etc/init.d/dbus start
-    exec cupsd -f
 elif [ "$1" = "cups-browsed" ]; then
     /etc/init.d/dbus start
     cupsd
-    exec "$@"
-else
-    exec "$@"
 fi
+
+exec "$@"
